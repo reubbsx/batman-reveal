45a158f48c3dfffccd11fab43e390addc8326b98
Reuben <reubenmathewsam666@gmail.com>
2026-03-16 19:53:39 +0000

    modified:   src/App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ const DIALOGUE_SEQUENCE = [
   { character: "ROBIN", text: "thats out of the script ,wtf , its meant to be a birthday gift , try again bruce ~~ , I mean Batman", sprite:process.env.PUBLIC_URL + "/robin-batman-nightwing-superhero-superhero-robin-png-picture-adfd21ac9551dcd80d63942004f37e10.png" },
   { character: "BATMAN", text: "Ahem okay okay , so thank you for being my friend so far(making you automatically one of the coolest people to walk on planet earth) , and although your birthday might be something far off , id want to say happy 27th birthday in advance :))) ", sprite: process.env.PUBLIC_URL + "/batman-the-man-who-laughs-joker-internet-meme-batman-7e1b1dfeb20fc4a4ab87b1071ec7e9aa.png" },
   { character: "ROBIN", text: "Anyways tell her whats up next , what do we do now , omg you suck", sprite:process.env.PUBLIC_URL + "/robin-batman-nightwing-superhero-superhero-robin-png-picture-adfd21ac9551dcd80d63942004f37e10.png" },
-  { character: "BATMAN", text: "hush, Anyways jit , you would have to head downstairs  to the amazon locker with the code that is shown next , and you will get your stuff, if theres any issues let me know unc , have a good one", sprite:process.env.PUBLIC_URL + "/63-638700_batman-the-animated-series.png" }];
+  { character: "BATMAN", text: "hush, Anyways jit ,you would have to head downstairs  to the amazon locker with the code that is shown next , and you will get your stuff, if theres any issues let me know unc , have a good one", sprite:process.env.PUBLIC_URL + "/63-638700_batman-the-animated-series.png" }];
  
 function App() {
   const [status, setStatus] = useState('LOCKED'); 
